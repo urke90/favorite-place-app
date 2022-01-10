5df367882c99455c23af1470015976cc11fd4c54
urke90 <urosbijelic90@gmail.com>
2022-01-10 16:59:03 +0100

    created user -item/llist components with dummy data

---
+++ client/src/user/components/UserItem.tsx
@@ -0,0 +1,10 @@
+const UserItem: React.FC<{
+    id: string;
+    image: string;
+    name: string;
+    placeCount: number;
+}> = ({ id, image, name, placeCount }) => {
+    return <div>UserItem </div>;
+};
+
+export default UserItem;
