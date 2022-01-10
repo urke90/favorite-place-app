4f7cd0cf2c9544e84e69c4cff4a9a78074cc0152
urke90 <urosbijelic90@gmail.com>
2022-01-10 14:20:45 +0100

    implemented basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-const App = () => {
-  return <h1>Let's start!</h1>;
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App: React.FC = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
 };
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
