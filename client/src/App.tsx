ebd3dbba8d3d5a6c26025b3edf16cd7e7ddfac98
urke90 <urosbijelic90@gmail.com>
2022-01-11 16:30:38 +0100

    added MainNavigation and mainHeader comps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,20 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from 'shared/components/navigation/MainNavigation';
 
 const App: React.FC = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Users />} />
-            <Route path="/places/new" element={<NewPlace />} />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <>
+            <MainNavigation />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Users />} />
+                    <Route path="/places/new" element={<NewPlace />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </main>
+        </>
     );
 };
 
