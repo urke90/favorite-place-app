e3d1fd141bff27ec9a01d84567ae725457a56520
urke90 <urosbijelic90@gmail.com>
2022-01-12 17:40:51 +0100

    added transtion to sidedrawe

--- client/src/shared/components/navigation/SideDrawer.tsx
+++ client/src/shared/components/navigation/SideDrawer.tsx
@@ -1,8 +1,20 @@
-import './SideDrawer.css';
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
-const SideDrawer: React.FC = ({ children }) => {
-    const content = <aside className="side-drawer">{children}</aside>;
+import './SideDrawer.css';
+
+const SideDrawer: React.FC<{ show: boolean }> = ({ children, show }) => {
+    const content = (
+        <CSSTransition
+            in={show}
+            timeout={200}
+            classNames="slide-in-left"
+            mountOnEnter
+            unmountOnExit
+        >
+            <aside className="side-drawer">{children}</aside>
+        </CSSTransition>
+    );
 
     return ReactDOM.createPortal(
         content,
