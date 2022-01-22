f263cbbd2c4d7206d4d4b3c79975be83ade75517
urke90 <urosbijelic90@gmail.com>
2022-01-22 17:19:32 +0100

    added logic for validating form in overall

--- client/src/places/pages/NewPlace.tsx
+++ client/src/places/pages/NewPlace.tsx
@@ -1,4 +1,7 @@
+import { useReducer, useCallback } from 'react';
+
 import Input from 'shared/components/FormElements/Input';
+import Button from 'shared/components/FormElements/Button';
 import {
     VALIDATOR_REQUIRE,
     VALIDATOR_MINLENGTH
@@ -6,10 +9,81 @@ import {
 
 import './NewPlace.css';
 
-const NewPlace = () => {
-    const inputChangeHandler = () => {
-        console.log('inputChangeHandler');
+type ActionType = {
+    type: 'INPUT_CHANGE';
+    id: string;
+    value: string;
+    isValid: boolean;
+};
+
+interface NewPlaceItem {
+    value: string;
+    isValid: boolean;
+}
+
+interface NewPlaceState {
+    inputs: {
+        [key: string]: NewPlaceItem;
     };
+
+    formIsValid: boolean;
+}
+
+const newPlaceInitState: NewPlaceState = {
+    inputs: {
+        title: {
+            value: '',
+            isValid: false
+        },
+        description: {
+            value: '',
+            isValid: false
+        }
+    },
+    formIsValid: false
+};
+
+const newPlaceReducer = (
+    state: NewPlaceState,
+    { type, id, value, isValid }: ActionType
+) => {
+    switch (type) {
+        case 'INPUT_CHANGE':
+            const newState = {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [id]: {
+                        value,
+                        isValid
+                    }
+                }
+            };
+
+            newState.formIsValid = !Object.values(newState.inputs).some(
+                (input) => input.isValid === false
+            );
+
+            return {
+                ...newState
+            };
+        default:
+            return state;
+    }
+};
+
+const NewPlace = () => {
+    const [newPlaceState, dispatch] = useReducer(
+        newPlaceReducer,
+        newPlaceInitState
+    );
+
+    const inputChangeHandler = useCallback(
+        (id: string, value: string, isValid: boolean) => {
+            dispatch({ type: 'INPUT_CHANGE', id, value, isValid });
+        },
+        []
+    );
 
     return (
         <form className="place-form">
@@ -30,6 +104,9 @@ const NewPlace = () => {
                 validators={[VALIDATOR_MINLENGTH(5)]}
                 onInputChange={inputChangeHandler}
             />
+            <Button disabled={!newPlaceState.formIsValid} type="submit">
+                ADD PLACE
+            </Button>
         </form>
     );
 };
