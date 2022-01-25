b89861e9681a20495c9ce28c2fd32ca4eed056a6
Uros Bijelic <36512483+urke90@users.noreply.github.com>
2022-01-25 17:04:32 +0100

    refactored interface for Card comp styles and logic in places comps if no items/users etc (#26)

--- client/src/shared/components/UI/Card.tsx
+++ client/src/shared/components/UI/Card.tsx
@@ -1,5 +1,8 @@
-import { CardProps } from 'models/card';
 import './Card.css';
+
+interface CardProps {
+    className?: string;
+}
 
 const Card: React.FC<CardProps> = ({ className, children }) => {
     return <div className={`card ${className}`}>{children}</div>;
