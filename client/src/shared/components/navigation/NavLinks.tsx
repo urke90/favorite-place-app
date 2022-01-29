11439806011b65e5e0ffca4dcf0d18e79e4990ea
urke90 <urosbijelic90@gmail.com>
2022-01-29 14:29:21 +0100

    toggle nav link depending on authentication status

--- client/src/shared/components/navigation/NavLinks.tsx
+++ client/src/shared/components/navigation/NavLinks.tsx
@@ -1,22 +1,33 @@
+import { useContext } from 'react';
+import { AuthContext } from 'context/AuthContext';
+
 import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
 
 const NavLinks: React.FC = () => {
+    const { isAuth } = useContext(AuthContext);
+
     return (
         <ul className="nav-links">
             <li>
                 <NavLink to="/">ALL USERS</NavLink>
             </li>
-            <li>
-                <NavLink to="/u1/places">MY PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">ADD PLACE</NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">AUTHENTICATE</NavLink>
-            </li>
+            {isAuth && (
+                <li>
+                    <NavLink to="/u1/places">MY PLACES</NavLink>
+                </li>
+            )}
+            {isAuth && (
+                <li>
+                    <NavLink to="/places/new">ADD PLACE</NavLink>
+                </li>
+            )}
+            {!isAuth && (
+                <li>
+                    <NavLink to="/auth">AUTHENTICATE</NavLink>
+                </li>
+            )}
         </ul>
     );
 };
