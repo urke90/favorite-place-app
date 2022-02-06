c87d0ed9165695e293caa1b7849805794b920475
urke90 <urosbijelic90@gmail.com>
2022-02-06 13:38:24 +0100

    added utils func that creates lat and lng based on user input

---
+++ server/src/utils/mapLocation.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const getAddressGeoLocation = async (address: string) => {
+    const MAPBOX_API_TOKEN = process.env.MAPBOX_API_TOKEN;
+
+    // https://docs.mapbox.com/api/search/geocoding/
+
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        address
+    )}.json?access_token=${MAPBOX_API_TOKEN}`;
+
+    try {
+        const result = await axios.get(url);
+
+        const [lat, lng] = result.data.features[0].geometry.coordinates;
+
+        return {
+            lat,
+            lng
+        };
+    } catch (error) {
+        // console.log('error get geolocation', error);
+    }
+};
