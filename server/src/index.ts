de66df5138ccfe3d8299b7a67d0c643401ecd2bc
urke90 <urosbijelic90@gmail.com>
2022-02-07 12:34:21 +0100

    init mongoo/mongoose setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express, { ErrorRequestHandler } from 'express';
 import cors from 'cors';
+import mongoose, { mongo } from 'mongoose';
 
 /**
- * Routes
- * DANGER =====>  check diff when importin ()
+ * * Routes
+ * ! DANGER =====>  check diff when importin ()
  */
+
 // import placeRoutes from 'routes/places-routes';
 // import userRoutes from './routes/users-routes';
 
@@ -15,9 +17,10 @@ import HttpError from './models/error/http-error';
 const app = express();
 
 /**
- * MIDDLEWARES
- *  ******* should we use body-parser instead of body-parser ******
+ * *MIDDLEWARES
+ * ? should we use body-parser instead of body-parser
  */
+
 app.use(express.json());
 app.use(
     cors({
@@ -42,4 +45,18 @@ const errorMiddleWare: ErrorRequestHandler = (error, req, res, next) => {
 
 app.use(errorMiddleWare);
 
-app.listen(process.env.PORT || 5000);
+/**
+ * * establish connection to the mongo db before we connect to the server
+ * ! mongodb+srv://urke90:xLTqhzl07eBkPKS7@visit-places.sqptd.mongodb.net/visit-places?retryWrites=true&w=majority
+ */
+
+mongoose
+    .connect(process.env.MONGO_DB_URL!)
+    .then(() => {
+        console.log('mongo connected');
+
+        app.listen(process.env.PORT || 5000);
+    })
+    .catch((err) => {
+        console.log('error connecting to Mongo mongoose');
+    });
