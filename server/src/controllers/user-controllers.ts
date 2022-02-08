7b1d1fb91799b18f2ecc023f12e533bd400d3ad5
urke90 <urosbijelic90@gmail.com>
2022-02-08 14:52:52 +0100

    updated login route/func

--- server/src/controllers/user-controllers.ts
+++ server/src/controllers/user-controllers.ts
@@ -1,40 +1,58 @@
 import { Request, Response, NextFunction } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import { validationResult } from 'express-validator';
 
 import HttpError from '../types/error/http-error';
 import { IUser } from 'types/user/user';
 import User from '../models/user';
 
-const DUMMY_USERS: IUser[] = [
-    {
-        id: 'u1',
-        name: 'Milos Degenek',
-        email: 'kurobecalo@gmail.com',
-        password: 'abc123'
-    },
-    {
-        id: 'u2',
-        name: 'Nemanja Bjelica',
-        email: 'bjelica@gmail.com',
-        password: '123abc'
-    }
-];
+// const DUMMY_USERS: IUser[] = [
+//     {
+//         id: 'u1',
+//         name: 'Milos Degenek',
+//         email: 'kurobecalo@gmail.com',
+//         password: 'abc123'
+//     },
+//     {
+//         id: 'u2',
+//         name: 'Nemanja Bjelica',
+//         email: 'bjelica@gmail.com',
+//         password: '123abc'
+//     }
+// ];
 
 export const getUsers = (req: Request, res: Response, next: NextFunction) => {
-    res.json({ users: DUMMY_USERS });
+    res.json({ users: 'fasdfads' });
 };
 
-export const usersLogin = (req: Request, res: Response, next: NextFunction) => {
+/**
+ * ***************************************************************************************************************
+ */
+
+export const usersLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const { email, password } = req.body;
 
-    const existingUser = DUMMY_USERS.find((u) => u.email === email);
+    let existingUser;
 
-    if (!existingUser || existingUser.password !== password)
-        throw new HttpError('User not found', 401);
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (err) {
+        return next(new HttpError('Login failed, please try again', 500));
+    }
+
+    if (!existingUser || existingUser.password !== password) {
+        return next(new HttpError('Invalid credentials', 401));
+    }
 
     res.json({ message: 'Logged in!' });
 };
+
+/**
+ * ***************************************************************************************************************
+ */
 
 export const usersSignup = async (
     req: Request,
@@ -52,7 +70,7 @@ export const usersSignup = async (
     let existingUser;
 
     try {
-        existingUser = await User.findOne({ email }).exec();
+        existingUser = await User.findOne({ email });
     } catch (err) {
         return next(new HttpError('Signup failed, please try again', 500));
     }
