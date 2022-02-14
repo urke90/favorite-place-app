be2cd2527d9db2b8802d23bad132bb656de0da80
urke90 <urosbijelic90@gmail.com>
2022-02-14 12:34:40 +0100

    added error modal for failed signup/login

--- client/src/auth/Auth.tsx
+++ client/src/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import useForm from 'hooks/use-form';
@@ -7,6 +7,8 @@ import useAuth from 'hooks/use-auth';
 import { FormState } from 'models/form/form';
 import Input from 'shared/components/FormElements/Input';
 import Button from 'shared/components/FormElements/Button';
+import Modal from 'shared/components/UI/Modals/Modal';
+import LoadingSpinner from 'shared/components/UI/LoadingSpinner';
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
@@ -14,11 +16,6 @@ import {
 } from 'util/validatiors';
 
 import './Auth.css';
-
-/**
- * figure out logic for login and signup handler
- *  ************* or the same function can be used for both but handle this on BE?!?!?!?
- */
 
 interface ILoginData {
     password: string;
@@ -61,6 +58,9 @@ const authSignupState: FormState = {
 };
 
 const Auth: React.FC = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<null | string>(null);
+
     const [isLoginMode, toggleLoginMode] = useToggle(true);
 
     const [authState, inputChangeHandler, setAuthFormData] =
@@ -75,7 +75,8 @@ const Auth: React.FC = () => {
 
         const { name, email, password } = authState.inputs;
         let baseUrl = '/api/users';
-        let url = baseUrl + '/login';
+        // let url = baseUrl + '/login';
+        let url = `${baseUrl}/${isLoginMode ? 'login' : 'signup'} `;
 
         let data: ISignupData | ILoginData = {
             email: email.value,
@@ -83,7 +84,6 @@ const Auth: React.FC = () => {
         };
 
         if (!isLoginMode) {
-            url = baseUrl + '/signup';
             data = {
                 name: name.value,
                 email: email.value,
@@ -92,17 +92,23 @@ const Auth: React.FC = () => {
         }
 
         try {
+            setIsLoading(true);
             const response = await axios.post(url, data);
-            console.log('response', response);
+            console.log('responseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', response);
+
             if (response.status !== 201) {
-                throw new Error(
-                    'Something went wrong. Please try again later.'
-                );
+                throw new Error("Can't create new user");
             }
-        } catch (err) {
-            console.log('err', err);
+            // CONTINUE HERE
+        } catch (err: any) {
+            setError(
+                err.message || 'Something went wrong, please try again later!'
+            );
         }
+        setIsLoading(false);
     };
+
+    const closeErrorModal = () => setError(null);
 
     useEffect(() => {
         if (isLoginMode) {
@@ -112,49 +118,60 @@ const Auth: React.FC = () => {
     }, [isLoginMode]);
 
     return (
-        <form className="auth-form" onSubmit={authSubmitHandler}>
-            {!isLoginMode && (
+        <>
+            <Modal
+                showModal={!!error}
+                onCloseModal={closeErrorModal}
+                header="Error occured!"
+                footer={<Button onClick={closeErrorModal}>CLOSE</Button>}
+            >
+                <p>{error && error}</p>
+            </Modal>
+            <form className="auth-form" onSubmit={authSubmitHandler}>
+                {isLoading && <LoadingSpinner asOverlay />}
+                {!isLoginMode && (
+                    <Input
+                        id="name"
+                        label="Name"
+                        element="input"
+                        type="text"
+                        placeholder="User name"
+                        errorText="Please enter name"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        onInputChange={inputChangeHandler}
+                    />
+                )}
                 <Input
-                    id="name"
-                    label="Name"
+                    id="email"
+                    label="Email"
                     element="input"
                     type="text"
-                    placeholder="User name"
-                    errorText="Please enter name"
-                    validators={[VALIDATOR_REQUIRE()]}
+                    placeholder="Email"
+                    errorText="Email is not valid. Please enter valid email"
+                    validators={[VALIDATOR_EMAIL()]}
                     onInputChange={inputChangeHandler}
                 />
-            )}
-            <Input
-                id="email"
-                label="Email"
-                element="input"
-                type="text"
-                placeholder="Email"
-                errorText="Email is not valid. Please enter valid email"
-                validators={[VALIDATOR_EMAIL()]}
-                onInputChange={inputChangeHandler}
-            />
-            <Input
-                id="password"
-                label="Password"
-                element="input"
-                type="password"
-                placeholder="Password"
-                errorText="Please enter valid password (6 characters long)"
-                validators={[VALIDATOR_MINLENGTH(6)]}
-                onInputChange={inputChangeHandler}
-            />
-            <div className="center column">
-                <Button disabled={!authState.formIsValid} type="submit">
-                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
-                </Button>
-                <br />
-                <Button inverse onClick={toggleLoginMode} type="button">
-                    SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-                </Button>
-            </div>
-        </form>
+                <Input
+                    id="password"
+                    label="Password"
+                    element="input"
+                    type="password"
+                    placeholder="Password"
+                    errorText="Please enter valid password (6 characters long)"
+                    validators={[VALIDATOR_MINLENGTH(6)]}
+                    onInputChange={inputChangeHandler}
+                />
+                <div className="center column">
+                    <Button disabled={!authState.formIsValid} type="submit">
+                        {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                    </Button>
+                    <br />
+                    <Button inverse onClick={toggleLoginMode} type="button">
+                        SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+                    </Button>
+                </div>
+            </form>
+        </>
     );
 };
 
---
+++ client/src/shared/components/UI/Modals/ErrorModal.tsx
@@ -0,0 +1,20 @@
+// import Modal from './Modal';
+// import Button from 'shared/components/FormElements/Button';
+
+// interface IErrorModalProps {}
+
+// const ErrorModal: React.FC = ({ onCloseModal, error }) => {
+//     return (
+//         <Modal
+//             onCloseModal={onCloseModal}
+//             header="An Error Occurred!"
+//             showModal={!!error}
+//             footer={<Button onClick={onCloseModal}>Okay</Button>}
+//         >
+//             <p>{error}</p>
+//         </Modal>
+//     );
+// };
+
+// export default ErrorModal;
+export default {};
