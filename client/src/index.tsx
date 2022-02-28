61f5cf81ea5b3d9329d9c91409786268b5d61fde
Uros Bijelic <36512483+urke90@users.noreply.github.com>
2022-02-28 19:23:17 +0100

    automatically login user if token is still valid in storage, and logout if it's not (#84)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import AuthContextProvider from 'context/AuthContext';
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
     </BrowserRouter>,
-
     document.getElementById('root')
 );
